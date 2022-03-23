7f276ce50571324e17af142c353bf10e70ab99d1
Dorian Frances <dorian.frances@diakse.com>
2022-03-23 11:48:08 +0100

    Creating first pages

---
+++ src/Controller/TestController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index() {
+        dump("Ca fonctionne");
+        die();
+    }
+
+    /**
+     * @Route("/test/{slug<\d+>?0}", name="test")
+     */
+    public function test(Request $request) : Response
+    {   
+        $age = $request->attributes->get('slug',0);
+        
+        return new Response("Vous avez $age ans !");
+    }
+}
